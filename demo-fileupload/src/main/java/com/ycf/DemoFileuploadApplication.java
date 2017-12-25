5ef4be6b81b016ebb9c73ba022450bdc8c2f8825
yangcf <836843022@qq.com>
2017-12-25 15:18:15 +0800

    springboot文件上传

---
+++ demo-fileupload/src/main/java/com/ycf/DemoFileuploadApplication.java
@@ -0,0 +1,12 @@
+package com.ycf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFileuploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFileuploadApplication.class, args);
+	}
+}
