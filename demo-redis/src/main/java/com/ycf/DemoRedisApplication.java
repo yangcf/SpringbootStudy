d53576e5e55d55c3df272bb9d76e4f5eeeef43cd
yangcf <836843022@qq.com>
2017-12-25 17:27:02 +0800

    springboot将Redis作为缓存

---
+++ demo-redis/src/main/java/com/ycf/DemoRedisApplication.java
@@ -0,0 +1,12 @@
+package com.ycf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRedisApplication.class, args);
+	}
+}
---
+++ demo-redis/src/main/java/com/ycf/config/RedisCacheConfig.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright 2017 超凡. All rights reserved
+ * Project Name:springboot-demo
+ * Module Name:Core
+ */
+package com.ycf.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * what:  redis作为缓存配置. <br/>
+ *
+ * @author 杨超凡 created on 2017/12/25
+ */
+@Configuration
+@EnableCaching //启用缓存
+public class RedisCacheConfig extends CachingConfigurerSupport {
+
+    /**
+     * 设置缓存管理器
+     *
+     * @param redisTemplate
+     * @return
+     */
+    @Bean
+    public CacheManager cacheManager(RedisTemplate<?, ?> redisTemplate) {
+        CacheManager cacheManager = new RedisCacheManager(redisTemplate);
+        return cacheManager;
+    }
+
+    /**
+     *这里的序列化方式需要修改（这里只是String类型的系列化）
+     * @param factory
+     * @return
+     */
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<String, String>();
+        redisTemplate.setConnectionFactory(factory);
+        RedisSerializer<String> redisSerializer = new StringRedisSerializer();//Long类型不可以会出现异常信息;
+        redisTemplate.setKeySerializer(redisSerializer);
+        redisTemplate.setHashKeySerializer(redisSerializer);
+        return redisTemplate;
+    }
+}
